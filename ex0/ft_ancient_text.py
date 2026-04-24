51271c37122e278be8a37b6b006ae62fe2fe5490
MusangQim <musangqim03@gmail.com>
2026-04-24 18:34:35 +0800

    ex0 added

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+import typing
+
+
+def main() -> None:
+    filename = sys.argv
+    if filename:
+        print("Usage: ft_ancient_text.py <file>\n")
+        exit()
+    else:
+        print("=== Cyber Archives Recovery ===")
+        print("Accessing file")
+
+
+
+if __name__ == "__main__":
+    main()
