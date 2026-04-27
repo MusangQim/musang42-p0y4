0977cf45ffda68e0cb6d3bd1d41c351a77bc235b
MusangQim <musangqim03@gmail.com>
2026-04-27 11:54:41 +0800

    ex1 added

---
+++ ex1/ft_archive_creation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import typing
+
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        print("Usage: ft_archive_creation.py <file>")
+        exit()
+    print("=== Cyber Archives Recovery & Preservation ===")
+    print(f"Accessing file '{sys.argv[1]}'")
+    try:        
+    except FileNotFoundError as a:
+        print(f"Error opening file '{sys.argv[1]}': {a}\n")
+    except PermissionError as b:
+        print(f"Error opening file '{sys.argv[1]}': {b}\n")
+
+
+if __name__ == "__main__":
+    main()
