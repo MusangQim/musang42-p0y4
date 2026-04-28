397fa91cabcec579b8647c7d50616632679e5d8c
MusangQim <musangqim03@gmail.com>
2026-04-28 17:04:44 +0800

    update: stderr

--- ex2/ft_stream_management.py
+++ ex2/ft_stream_management.py
@@ -2,11 +2,53 @@
 import sys
 import typing
 
+
 def main() -> None:
     if len(sys.argv) < 2:
         print("Usage: ft_stream_management.py <file>\n")
         exit()
     print("=== Cyber Archives Recovery & Preservation ===")
+    print(f"Accessing file '{sys.argv[1]}'")
+    try:
+        # --- FILE OPEN ---
+        file: typing.IO = open(sys.argv[1], "r")
+        # --- FILE READ ---
+        content = file.read()
+        print("---\n")
+        print(content)
+        print("\n---")
+        # --- FILE CLOSE ---
+        file.close()
+        print(f"File '{sys.argv[1]}' closed.\n")
+
+        # --- TRANSFORMING FILE ADDED '#' ---
+        print("Transform data:")
+        print("---\n")
+        lines = content.split('\n')
+        transformed = []
+        for line in lines:
+            new_line = line + '#'
+            transformed.append(new_line)
+        for line in transformed:
+            print(line)
+        print("\n---")
+        # --- ENTERING INPUT FOR NEW FILE NAME ---
+        new_input = input("Enter new file name (or empty): ")
+        if new_input:
+            new_file: typing.IO = open(new_input, "w")
+            print(f"Saving data to '{new_input}'")
+            new_file.write('\n'.join(transformed))
+            new_file.close()
+            print(f"Data saved in file '{new_input}'")
+        else:
+            print("Not saving data.")
+    except FileNotFoundError as a:
+        sys.stderr.write(f"Error opening file '{sys.argv[1]}': {a}\n")
+        sys.stderr.flush()
+    except PermissionError as b:
+        sys.stderr.write(f"Error opening file '{sys.argv[1]}': {b}\n")
+        sys.stderr.flush()
+
 
 if __name__ == "__main__":
     main()
