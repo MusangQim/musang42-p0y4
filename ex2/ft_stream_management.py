14c5f9ed5e1108f381b664e1bd98251e067ecb1f
MusangQim <musangqim03@gmail.com>
2026-04-28 16:06:00 +0800

    ex2 added

---
+++ ex2/ft_stream_management.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import typing
+
+def main() -> None:
+    if len(sys.argv) < 2:
+        print("Usage: ft_stream_management.py <file>\n")
+        exit()
+    print("=== Cyber Archives Recovery & Preservation ===")
+
+if __name__ == "__main__":
+    main()
